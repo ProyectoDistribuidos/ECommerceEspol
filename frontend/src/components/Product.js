b6de9cb57f00df45e368a5a05f31d2b5743e5637
Darjotri <45737453+Darjotri@users.noreply.github.com>
2021-01-18 05:17:29 -0500

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -20,9 +20,6 @@ export default function Product(props) {
         <div className="row">
           <div className="price">${product.price}</div>
           <div>
-            <Link to={`/seller/${product.seller._id}`}>
-              {product.seller.seller.name}
-            </Link>
           </div>
         </div>
       </div>
